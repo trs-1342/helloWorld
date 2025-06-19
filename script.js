b0873c67ddef293f1ed7ccac225faa6a552d87c1
halil <socialmediaoftrs1342@gmail.com>
2025-06-20 00:58:14 +0300

    courser

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  const status = document.querySelector(".form-status");
-  status.textContent = "Mesaj gönderildi! (demo)";
-  setTimeout(() => {
-    status.textContent = "";
-    this.reset();
-  }, 3000);
-});
+// document.getElementById("contactForm").addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const status = document.querySelector(".form-status");
+//   status.textContent = "Mesaj gönderildi! (demo)";
+//   setTimeout(() => {
+//     status.textContent = "";
+//     this.reset();
+//   }, 3000);
+// });
+
+// const cursor = document.querySelector(".custom-cursor");
+// document.addEventListener("mousemove", (e) => {
+//   cursor.style.left = e.clientX + "px";
+//   cursor.style.top = e.clientY + "px";
+// });
